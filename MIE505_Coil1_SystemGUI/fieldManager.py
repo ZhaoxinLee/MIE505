da790c7b145edc11997d6cdd1abf8c040cb07bf1
Zhaoxin Li <43050503+ZhaoxinLee@users.noreply.github.com>
2023-08-10 19:09:27 -0400

    Add files via upload

---
+++ MIE505_Coil1_SystemGUI/fieldManager.py
@@ -0,0 +1,97 @@
+
+class FieldManager(object):
+    def __init__(self, dac):
+        # Output PWM frequency
+        self.pwmFreq = 500 # Hz
+        # Pin numbers for the pwm and direction outputs
+        self.pinXpwm = 17
+        self.pinXdir = 27
+        self.pinYpwm = 23
+        self.pinYdir = 24
+        self.pinZpwm = 25
+        self.pinZdir = 8
+        # Object for controlling RPi GPIO using gpiozero
+        self.dac = dac
+        # Set up GPIO for coil control
+        self.dac.pi.set_PWM_frequency(self.pinXpwm, self.pwmFreq)
+        self.dac.pi.set_PWM_frequency(self.pinYpwm, self.pwmFreq)
+        self.dac.pi.set_PWM_frequency(self.pinYpwm, self.pwmFreq)
+        self.dac.set_pin_to_write_mode(self.pinXdir)
+        self.dac.set_pin_to_write_mode(self.pinYdir)
+        self.dac.set_pin_to_write_mode(self.pinZdir)
+        # Coil analog output conversion factors determined from coil
+        # calibration.
+        self.aoFactorX = 0.746 #[mT/V]
+        self.aoFactorY = 1.190 #[mT/V]
+        self.aoFactorZ = 2.038 #[mT/V]
+        # Coil analog conversion factors determined from coil
+        # calibration
+        self.aiFactorX = 0.767 #[mT/A]
+        self.aiFactorY = 0.755 #[mT/A]
+        self.aiFactorZ = 0.720 #[mT/A]
+        # Coil cutoff frequencies determined from coil calibration
+        self.freqCutoffX = 13.0 #[Hz]
+        self.freqCutoffY = 17.9 #[Hz]
+        self.freqCutoffZ = 28.7 #[Hz]
+        # Power supply voltage
+        self.VsupplyX = 24.14 #[V]
+        self.VsupplyY = 24.12 #[V]
+        self.VsupplyZ = 24.15 #[V]
+        # Initial requested field values
+        self.bxSetpoint = 0 #[mT]
+        self.bySetpoint = 0 #[mT]
+        self.bzSetpoint = 0 #[mT]
+        # Initial field values estimated from measured coil currents
+        self.bxEstimate = 0 #[mT]
+        self.byEstimate = 0 #[mT]
+        self.bzEstimate = 0 #[mT]
+        self.ix1 = 0
+        self.ix2 = 0
+        self.iy1 = 0
+        self.iy2 = 0
+        self.iz1 = 0
+        self.iz2 = 0
+
+
+    def setX(self, mT):
+        """Generate a zero-gradient magnetic flux density in the x-direction.
+        """
+        # The coils are controlled via PWM output from the Raspberry Pi
+        # that is amplified by a motor driver. The duty cycle of the PWM
+        # output is determined based on the desired voltage and the
+        # supply voltage.
+        self.dac.output_voltage_pwm(self.pinXpwm, self.pinXdir, mT / self.aoFactorX, self.VsupplyX)
+        self.bxSetpoint = mT
+
+    def setY(self, mT):
+        """Generate a zero-gradient magnetic flux density in the x-direction.
+        """
+        # The coils are controlled via PWM output from the Raspberry Pi
+        # that is amplified by a motor driver. The duty cycle of the PWM
+        # output is determined based on the desired voltage and the
+        # supply voltage.
+        self.dac.output_voltage_pwm(self.pinYpwm, self.pinYdir, mT / self.aoFactorY, self.VsupplyY)
+        self.bySetpoint = mT
+
+    def setZ(self, mT):
+        """Generate a zero-gradient magnetic flux density in the x-direction.
+        """
+        # The coils are controlled via PWM output from the Raspberry Pi
+        # that is amplified by a motor driver. The duty cycle of the PWM
+        # output is determined based on the desired voltage and the
+        # supply voltage.
+        self.dac.output_voltage_pwm(self.pinZpwm, self.pinZdir, mT / self.aoFactorZ, self.VsupplyZ)
+        self.bzSetpoint = mT
+
+    def setXYZ(self, x_mT, y_mT, z_mT):
+        """Generate a zero-gradient magnetic flux density in xyz."""
+        self.setX(x_mT)
+        self.setY(y_mT)
+        self.setZ(z_mT)
+
+    def getXYZ(self): #fake estimate
+        # Calculate an estimate of the magnetic flux density in mT from the
+        # measured coil currents.
+        self.bxEstimate = self.bxSetpoint
+        self.byEstimate = self.bySetpoint
+        self.bzEstimate = self.bzSetpoint
